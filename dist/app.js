896a21a02f0e0c579010d36fd722113549752f6e
Volodymyr Labunets <91912993+vplabunets@users.noreply.github.com>
2023-11-01 18:33:11 +0200

    add hw20 fibonachi TS

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,13 @@
 "use strict";
+function fib(input = 1) {
+    let arrFib = [];
+    for (let i = 0; i < input; i += 1) {
+        if (i < 2)
+            arrFib.push(i);
+        else
+            arrFib.push(arrFib[i - 1] + arrFib[i - 2]);
+    }
+    return arrFib[input - 1];
+}
+console.log(fib(30));
 //# sourceMappingURL=app.js.map
