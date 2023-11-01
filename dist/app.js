c91e26f0840f851afdb64b0be9f51e476cd9a36e
Volodymyr Labunets <91912993+vplabunets@users.noreply.github.com>
2023-11-01 18:03:26 +0200

    add hw-19TS

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,2 @@
 "use strict";
-const arr = [1, 2, 3, -1, -2, -3];
-function positiveElFilter(arr) {
-    const filteredArray = [];
-    if (arr.length === 0)
-        throw new Error("Input array is empty");
-    for (let i = 0; i < arr.length; i += 1) {
-        if (arr[i] > 0) {
-            filteredArray.push(arr[i]);
-        }
-    }
-    return filteredArray.length === 0 ? null : filteredArray;
-}
-console.log(positiveElFilter(arr));
 //# sourceMappingURL=app.js.map
